beb5ab219cff9e9241b3f866dc183e74cbcea6af
SChen <hugochen1024@gmail.com>
2020-04-21 10:11:27 +0800

    add opencv-3-图片存储与相对路径

--- main.cpp
+++ main.cpp
@@ -14,17 +14,19 @@ int main(int argc, char *argv[])
 
 
     // 设置 要显示的图像路径
-    std::string test_pic = "D:\\Project\\Vision\\ImageLab\\TestImages\\lena.png";
+    std::string test_pic = "../TestImages/lena.png";
 
     // 读取图像
     cv::Mat lena_img = cv::imread(test_pic);
 
     // 显示图像
     cv::imshow("图像显示窗口", lena_img);
-    cv::waitKey(0);
+    // cv::waitKey(100);
 
-    return 0;
+    // 存储图像
+    std::string write_pic = "../TestImages/lenalena_write.png";
 
+    cv::imwrite(write_pic,lena_img);
 
     return a.exec();
 }
