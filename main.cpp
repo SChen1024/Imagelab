5f837dcb98c63b26f32e8edc55f855d6bfb93784
SChen <hugochen1024@gmail.com>
2020-04-22 12:21:48 +0800

    feat: minus image

--- main.cpp
+++ main.cpp
@@ -15,36 +15,22 @@ int main(int argc, char *argv[])
     cv::Mat panda_bgr = cv::imread(img_panda);
     cv::Mat lena_bgr = cv::imread(img_lena);
     // 声明结果图像 1020*1020
-    cv::Mat res_bgr = cv::Mat::zeros(cv::Size(1024,1024), CV_8UC3);
+    cv::Mat res_bgr = cv::Mat::zeros(cv::Size(512,512), CV_8UC3);
 
     for (int i = 0; i < lena_bgr.rows; i++)
     {
         for (int j = 0; j < lena_bgr.cols; j++)
         {
-            // 复制第一副图像
-            res_bgr.at<cv::Vec3b>(i, j)[0] = (panda_bgr.at<cv::Vec3b>(i, j)[0]);
-            res_bgr.at<cv::Vec3b>(i, j)[1] = (panda_bgr.at<cv::Vec3b>(i, j)[1]);
-            res_bgr.at<cv::Vec3b>(i, j)[2] = (panda_bgr.at<cv::Vec3b>(i, j)[2]);
-
-            // 在第一副图下面 拼接 反色图像
-            res_bgr.at<cv::Vec3b>(512+i, j)[0] = (255- panda_bgr.at<cv::Vec3b>(i, j)[0]);
-            res_bgr.at<cv::Vec3b>(512+i, j)[1] = (255 - panda_bgr.at<cv::Vec3b>(i, j)[1]);
-            res_bgr.at<cv::Vec3b>(512+i, j)[2] = (255 -panda_bgr.at<cv::Vec3b>(i, j)[2]);
-
-            // 复制第二幅图像 
-            res_bgr.at<cv::Vec3b>(i, 512+j)[0] = (lena_bgr.at<cv::Vec3b>(i, j)[0]);
-            res_bgr.at<cv::Vec3b>(i, 512+j)[1] = (lena_bgr.at<cv::Vec3b>(i, j)[1]);
-            res_bgr.at<cv::Vec3b>(i, 512+j)[2] = (lena_bgr.at<cv::Vec3b>(i, j)[2]);
-
-            // 在第二副图下面 拼接 反色图像
-            res_bgr.at<cv::Vec3b>(512 + i, 512+j)[0] = (255 - lena_bgr.at<cv::Vec3b>(i, j)[0]);
-            res_bgr.at<cv::Vec3b>(512 + i, 512+j)[1] = (255 - lena_bgr.at<cv::Vec3b>(i, j)[1]);
-            res_bgr.at<cv::Vec3b>(512 + i, 512+j)[2] = (255 - lena_bgr.at<cv::Vec3b>(i, j)[2]);
-
+            // 求出最小值
+            cv::Vec3b tmp_px = lena_bgr.at<cv::Vec3b>(i, j);
+            int min_c = std::min(std::min(tmp_px[0], tmp_px[1]), tmp_px[2]);
+            
+            // 每个通道减去最小值
+            res_bgr.at<cv::Vec3b>(i, j)[0] = tmp_px[0] - min_c;
+            res_bgr.at<cv::Vec3b>(i, j)[1] = tmp_px[1] - min_c;
+            res_bgr.at<cv::Vec3b>(i, j)[2] = tmp_px[2] - min_c;
         }
     }
-
-    cv::imshow("panda_bgr", panda_bgr);
     cv::imshow("lena_bgr", lena_bgr);
     cv::imshow("res_bgr", res_bgr);
 
