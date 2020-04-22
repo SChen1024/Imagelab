af81537a5a6c008a1dc2e5080240c300bc7b0e0f
SChen <hugochen1024@gmail.com>
2020-04-22 12:49:19 +0800

    feat: add lena gamma corrt

--- main.cpp
+++ main.cpp
@@ -8,16 +8,23 @@ int main(int argc, char *argv[])
     //MainWindow w;
     //w.show();
     // 设置 要显示的图像路径
-    std::string img_panda = "./TestImages/panda.png";
     std::string img_lena = "./TestImages/lena.png";
 
     // 读取两幅彩色图像  512*512
-    cv::Mat panda_bgr = cv::imread(img_panda);
     cv::Mat lena_bgr = cv::imread(img_lena);
     // 声明结果图像 1020*1020
     cv::Mat res_bgr = cv::Mat::zeros(cv::Size(512,512), CV_8UC3);
 
-    float a = 2.2f, b = 50;
+    // 自定义 gamma 参数
+    float gamma = 0.4;
+
+    // 生成gamma 查找表
+    uchar table[256] = { 0 };
+    for (int i = 0; i < 256; i++)
+    {
+        table[i] = std::pow(i / 255.0f, gamma) * 255;
+    }
+
     for (int i = 0; i < lena_bgr.rows; i++)
     {
         for (int j = 0; j < lena_bgr.cols; j++)
@@ -25,9 +32,9 @@ int main(int argc, char *argv[])
             // 取出原始图像 灰度值
             cv::Vec3b tmp_px = lena_bgr.at<cv::Vec3b>(i, j);  
             // 每个通道减去最小值
-            res_bgr.at<cv::Vec3b>(i, j)[0] = cv::saturate_cast<uchar>(a * tmp_px[0] + b);
-            res_bgr.at<cv::Vec3b>(i, j)[1] = cv::saturate_cast<uchar>(a * tmp_px[1] + b);
-            res_bgr.at<cv::Vec3b>(i, j)[2] = cv::saturate_cast<uchar>(a * tmp_px[2] + b);
+            res_bgr.at<cv::Vec3b>(i, j)[0] = table[tmp_px[0]];
+            res_bgr.at<cv::Vec3b>(i, j)[1] = table[tmp_px[1]];
+            res_bgr.at<cv::Vec3b>(i, j)[2] = table[tmp_px[2]];
         }
     }
     cv::imshow("lena_bgr", lena_bgr);
