8c36cf51aef2807244d07ac40bc2a30dd3230fba
SChen <hugochen1024@gmail.com>
2020-04-24 21:10:35 +0800

    add basic drawing

--- main.cpp
+++ main.cpp
@@ -15,30 +15,13 @@ int main(int argc, char *argv[])
     // 声明结果图像 1020*1020
     cv::Mat res_bgr = cv::Mat::zeros(cv::Size(512,512), CV_8UC3);
 
-    // 自定义 gamma 参数
-    float gamma = 0.4;
+    // 绘制基本图形
+    cv::line(lena_bgr, cv::Point(100, 200), cv::Point(500, 300), cv::Scalar(0, 255, 0));
 
-    // 生成gamma 查找表
-    uchar table[256] = { 0 };
-    for (int i = 0; i < 256; i++)
-    {
-        table[i] = std::pow(i / 255.0f, gamma) * 255;
-    }
+    cv::line(lena_bgr, cv::Point(100, 200), cv::Point(500, 300), cv::Scalar(0, 255, 0), 1, 8, 1);
+    cv::line(lena_bgr, cv::Point(100, 200), cv::Point(500, 300), cv::Scalar(0, 255, 0), 1, 8, 2);
+    cv::line(lena_bgr, cv::Point(100, 200), cv::Point(500, 300), cv::Scalar(0, 255, 0), 1, 8, 3);
 
-    for (int i = 0; i < lena_bgr.rows; i++)
-    {
-        for (int j = 0; j < lena_bgr.cols; j++)
-        {
-            // 取出原始图像 灰度值
-            cv::Vec3b tmp_px = lena_bgr.at<cv::Vec3b>(i, j);  
-            // 每个通道减去最小值
-            res_bgr.at<cv::Vec3b>(i, j)[0] = table[tmp_px[0]];
-            res_bgr.at<cv::Vec3b>(i, j)[1] = table[tmp_px[1]];
-            res_bgr.at<cv::Vec3b>(i, j)[2] = table[tmp_px[2]];
-        }
-    }
-    cv::imshow("lena_bgr", lena_bgr);
-    cv::imshow("res_bgr", res_bgr);
 
     cv::waitKey(0);
 
