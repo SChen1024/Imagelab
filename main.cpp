03d8e15ab62012fc2422e2fe883a22fde7035297
SChen <hugochen1024@gmail.com>
2020-04-25 13:09:03 +0800

    feat: add mouse drawing
    鼠标绘制
    自定义颜色绘制

--- main.cpp
+++ main.cpp
@@ -2,6 +2,45 @@
 #include <QApplication>
 // 引入 opencv 函数头文件
 #include <opencv2/opencv.hpp>
+
+// 记录鼠标位置点, 以及 正在绘图标志位 flg 
+cv::Point start_p(-1, -1), end_p(-1, -1);
+bool flg_drawing = false;   
+
+// 使用原始图像与临时图像 存储
+cv::Mat src_img, temp_img;
+ 
+//鼠标回调函数 // 记录窗口的x y 位置 
+void on_MouseHandle(int event, int x, int y, int flags, void *param)
+{
+    switch (event)
+    {
+    case cv::EVENT_LBUTTONDOWN:
+    {
+        start_p = cv::Point(x, y);      // 确定起始点
+        temp_img = src_img.clone();     // 复制原始图, 进行绘图操作
+        flg_drawing = true;
+    }break;
+    case cv::EVENT_MOUSEMOVE:
+    {
+        if (flg_drawing)
+            end_p = cv::Point(x, y);        // 如果在绘制, 则更新移动后的目标点
+    }break;
+    case cv::EVENT_LBUTTONUP:
+    {
+        end_p = cv::Point(x, y);        // 确定最终点 
+        src_img = temp_img.clone();     // 将图像更新成为原始图 存储下来
+        flg_drawing = false;
+    }break;
+    }
+}
+
+// 返回两点之间的距离 直线距离 平方和的开方值
+float distance(const cv::Point &p1, const cv::Point &p2)
+{
+    return cv::sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
+}
+
 int main(int argc, char *argv[])
 {
     //QApplication a(argc, argv);
@@ -9,22 +48,46 @@ int main(int argc, char *argv[])
     //w.show();
     // 设置 要显示的图像路径
     std::string img_lena = "./TestImages/lena.png";
+    src_img = cv::imread(img_lena);
 
-    // 读取两幅彩色图像  512*512
-    cv::Mat lena_bgr = cv::imread(img_lena);
-    // 声明结果图像 1020*1020
-    cv::Mat res_bgr = cv::Mat::zeros(cv::Size(512,512), CV_8UC3);
+    std::string windows_name = "show";
+    cv::namedWindow(windows_name,cv::WINDOW_AUTOSIZE);
 
-    // 绘制基本图形
-    cv::line(lena_bgr, cv::Point(100, 200), cv::Point(500, 300), cv::Scalar(0, 255, 0));
+    // 设置窗口 鼠标操作 监听 函数为 on_MouseHandle
+    cv::setMouseCallback(windows_name, on_MouseHandle, 0);
 
-    cv::line(lena_bgr, cv::Point(100, 200), cv::Point(500, 300), cv::Scalar(0, 255, 0), 1, 8, 1);
-    cv::line(lena_bgr, cv::Point(100, 200), cv::Point(500, 300), cv::Scalar(0, 255, 0), 1, 8, 2);
-    cv::line(lena_bgr, cv::Point(100, 200), cv::Point(500, 300), cv::Scalar(0, 255, 0), 1, 8, 3);
+    //初始化随机种子
+    cv::RNG rng(time(0));
+
+    while (true)
+    {
+        // 根据当前点 绘制
+        if (flg_drawing)
+        {
+            temp_img = src_img.clone();
+
+            cv::line(temp_img, start_p, end_p, cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)));
+            cv::rectangle(temp_img, cv::Rect(start_p, end_p), cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)));
+            cv::circle(temp_img, start_p, distance(start_p,end_p), cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)));
+            cv::imshow(windows_name, temp_img);
+        }
+        else
+        {
+            cv::imshow(windows_name, src_img);
+        }
 
 
-    cv::waitKey(0);
+        // 设置 按 esc 退出循环
+        if (cv::waitKey(30) == 27)
+            break;
+    }
 
     return 0;
     // return a.exec();
 }
+
+
+
+
+
+
