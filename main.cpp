6377bba6711e5d1607cf208ef133bf5943eb91d0
SChen <hugochen1024@gmail.com>
2020-04-27 19:13:30 +0800

    feat: add filter2D comp sarpen

--- main.cpp
+++ main.cpp
@@ -3,91 +3,81 @@
 // 引入 opencv 函数头文件
 #include <opencv2/opencv.hpp>
 
-// 记录鼠标位置点, 以及 正在绘图标志位 flg 
-cv::Point start_p(-1, -1), end_p(-1, -1);
-bool flg_drawing = false;   
-
-// 使用原始图像与临时图像 存储
-cv::Mat src_img, temp_img;
- 
-//鼠标回调函数 // 记录窗口的x y 位置 
-void on_MouseHandle(int event, int x, int y, int flags, void *param)
+// 进行 测试 算法
+cv::Mat testFunc(const cv::Mat &src_img)
 {
-    switch (event)
+    cv::Mat res_img = cv::Mat::zeros(src_img.size(), CV_8UC1);
+
+    for (int i = 1; i < src_img.rows - 1; i++)
     {
-    case cv::EVENT_LBUTTONDOWN:
-    {
-        start_p = cv::Point(x, y);      // 确定起始点
-        temp_img = src_img.clone();     // 复制原始图, 进行绘图操作
-        flg_drawing = true;
-    }break;
-    case cv::EVENT_MOUSEMOVE:
-    {
-        if (flg_drawing)
-            end_p = cv::Point(x, y);        // 如果在绘制, 则更新移动后的目标点
-    }break;
-    case cv::EVENT_LBUTTONUP:
-    {
-        end_p = cv::Point(x, y);        // 确定最终点 
-        src_img = temp_img.clone();     // 将图像更新成为原始图 存储下来
-        flg_drawing = false;
-    }break;
+        for (int j = 1; j < src_img.cols - 1; j++)
+        {
+            res_img.at<uchar>(i, j) = cv::saturate_cast<uchar>(src_img.at<uchar>(i, j)
+                + src_img.at<uchar>(i, j) - src_img.at<uchar>(i - 1, j)
+                + src_img.at<uchar>(i, j) - src_img.at<uchar>(i + 1, j)
+                + src_img.at<uchar>(i, j) - src_img.at<uchar>(i, j - 1)
+                + src_img.at<uchar>(i, j) - src_img.at<uchar>(i, j + 1));
+        }
     }
+    return res_img;
 }
-
-// 返回两点之间的距离 直线距离 平方和的开方值
-float distance(const cv::Point &p1, const cv::Point &p2)
+// 使用测试 指针函数
+cv::Mat testFunc2(const cv::Mat &src_img)
 {
-    return cv::sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
+    cv::Mat res_img = cv::Mat::zeros(src_img.size(), CV_8UC1);
+
+    for (int i = 1; i < src_img.rows - 1; i++)
+    {
+        const uchar* p_row_pre = src_img.ptr<uchar>(i - 1);
+        const uchar* p_row_cur = src_img.ptr<uchar>(i);
+        const uchar* p_row_next = src_img.ptr<uchar>(i + 1);
+
+        uchar* p_row_res = res_img.ptr<uchar>(i);
+        for (int j = 1; j < src_img.cols - 1; j++)
+        {
+            *p_row_res++ = cv::saturate_cast<uchar>(5 * p_row_cur[j]
+                - p_row_cur[j-1] - p_row_cur[j+1] - p_row_pre[j] - p_row_next[j]);
+        }
+    }
+    return res_img;
 }
 
 int main(int argc, char *argv[])
 {
-    //QApplication a(argc, argv);
-    //MainWindow w;
-    //w.show();
     // 设置 要显示的图像路径
-    std::string img_lena = "./TestImages/lena.png";
-    src_img = cv::imread(img_lena);
+    std::string lena_png = "./TestImages/lena.png";
+    cv::Mat src_img = cv::imread(lena_png);
+    cv::cvtColor(src_img, src_img, cv::COLOR_BGR2GRAY);
 
-    std::string windows_name = "show";
-    cv::namedWindow(windows_name,cv::WINDOW_AUTOSIZE);
+    // 测试索引方式进行 锐化运算
+    double t = (double)cv::getTickCount();
+    cv::Mat res_img = testFunc(src_img);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    std::cout << "sharpen-index: \t\t" << t << std::endl;
 
-    // 设置窗口 鼠标操作 监听 函数为 on_MouseHandle
-    cv::setMouseCallback(windows_name, on_MouseHandle, 0);
+    // 测试 指针方式进行 锐化运算
+    t = (double)cv::getTickCount();
+    res_img = testFunc2(src_img);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    std::cout << "sharpen-pointer: \t" << t << std::endl;
+    
+    cv::Mat kernel = (cv::Mat_<char>(3, 3) << 0, -1, 0,
+        -1, 5, -1,
+        0, -1, 0);
+    cv::Mat res_img2;
 
-    //初始化随机种子
-    cv::RNG rng(time(0));
+    // 测试 filter 2D 算法时间
+    t = (double)cv::getTickCount();
+    cv::filter2D(src_img, res_img2, src_img.depth(), kernel);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    std::cout << "sharpen-filter: \t" << t << std::endl;
 
-    while (true)
-    {
-        // 根据当前点 绘制
-        if (flg_drawing)
-        {
-            temp_img = src_img.clone();
-
-            cv::line(temp_img, start_p, end_p, cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)));
-            cv::rectangle(temp_img, cv::Rect(start_p, end_p), cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)));
-            cv::circle(temp_img, start_p, distance(start_p,end_p), cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)));
-            cv::imshow(windows_name, temp_img);
-        }
-        else
-        {
-            cv::imshow(windows_name, src_img);
-        }
-
-
-        // 设置 按 esc 退出循环
-        if (cv::waitKey(30) == 27)
-            break;
-    }
-
+    cv::imshow("src_img", src_img);
+    cv::imshow("res_img", res_img);
+    cv::imshow("res_img2", res_img2);
+    cv::waitKey(0);
     return 0;
     // return a.exec();
 }
-
-
-
-
 
 
